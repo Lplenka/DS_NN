f61f1cd57c1a3b80727c3825027bd71b493420bc
Lplenka <laluprasad.lenka95@gmail.com>
2017-12-30 02:31:15 +0530

    Importing the libraries and dataset

---
+++ Data_Preprocessing/Data.py
@@ -0,0 +1,9 @@
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 3].values
