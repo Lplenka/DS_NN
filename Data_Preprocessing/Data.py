836cbecee687f440c7a8c1c1bb433f33fae3a47b
Lplenka <laluprasad.lenka95@gmail.com>
2017-12-30 02:46:14 +0530

    replaced missing data with mean

--- Data_Preprocessing/Data.py
+++ Data_Preprocessing/Data.py
@@ -11,3 +11,9 @@ y = dataset.iloc[:, 3].values
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Taking care of missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
