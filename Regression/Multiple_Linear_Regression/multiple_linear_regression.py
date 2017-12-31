5f434b02364ebdbf01ad7437b3e8321682ce0c8e
Lplenka <laluprasad.lenka95@gmail.com>
2018-01-01 05:00:46 +0530

    avoiding dummy variable trap

--- Regression/Multiple_Linear_Regression/multiple_linear_regression.py
+++ Regression/Multiple_Linear_Regression/multiple_linear_regression.py
@@ -16,3 +16,6 @@ labelencoder = LabelEncoder()
 X[:, 3] = labelencoder.fit_transform(X[:, 3])
 onehotencoder = OneHotEncoder(categorical_features = [3])
 X = onehotencoder.fit_transform(X).toarray()
+
+# Avoiding the Dummy Variable Trap
+X = X[:, 1:]
