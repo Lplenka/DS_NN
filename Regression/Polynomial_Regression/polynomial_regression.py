1afd8b29ac9ddfe23330236eb95d5264c0c979a7
Lplenka <laluprasad.lenka95@gmail.com>
2018-01-01 20:59:36 +0530

    not spliting the data this time

--- Regression/Polynomial_Regression/polynomial_regression.py
+++ Regression/Polynomial_Regression/polynomial_regression.py
@@ -11,9 +11,9 @@ X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
+"""from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
-
+"""
 # Feature Scaling
 """from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
