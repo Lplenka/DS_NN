dbb59ce4b82335fb064dfd50911f2441cf1c1aec
Lplenka <laluprasad.lenka95@gmail.com>
2018-01-02 18:25:25 +0530

    feature scaling

--- Regression/Support_Vector_Regression/support_vector_regression.py
+++ Regression/Support_Vector_Regression/support_vector_regression.py
@@ -9,3 +9,10 @@ import pandas as pd
 dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+sc_y = StandardScaler()
+X = sc_X.fit_transform(X)
+y = sc_y.fit_transform(y)
