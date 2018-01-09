268e641a546b1548fa28f790ef290d65f18041f1
Lplenka <laluprasad.lenka95@gmail.com>
2018-01-09 15:00:00 +0530

    Fit & Predict

--- Regression/Decision_Tree_Regression/Decision_Tree_Regression.py
+++ Regression/Decision_Tree_Regression/Decision_Tree_Regression.py
@@ -9,3 +9,11 @@ import pandas as pd
 dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
+
+# Fitting Decision Tree Regression to the dataset
+from sklearn.tree import DecisionTreeRegressor
+regressor = DecisionTreeRegressor(random_state = 0)
+regressor.fit(X, y)
+
+# Predicting a new result
+y_pred = regressor.predict(6.5)
