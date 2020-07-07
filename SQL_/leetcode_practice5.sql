a00f24f85e0e938cf3ae327eb050d5cdef04765a
lplenka <lalu.lenka@aptusdatalabs.com>
2020-07-07 19:15:32 +0530

    Added answer to question 5

--- SQL_/leetcode_practice5.sql
+++ SQL_/leetcode_practice5.sql
@@ -1,1 +1,28 @@
 -- https://leetcode.com/problems/human-traffic-of-stadium/
+# Write your MySQL query statement below
+
+select t.id,t.visit_date, t.people from
+
+(
+    
+ select id, visit_date, people, 
+ LAG(people,2) over w as 'people_2',
+ LAG(people,1) over w as 'people_1',
+ LEAD(people,1) over w as 'people1',
+ LEAD(people,2) over w as 'people2' 
+ from stadium
+ window w as (order by id) 
+
+) t
+
+where (t.people>=100
+and  t.people1>=100
+and t.people2>=100) 
+or
+(t.people>=100
+and  t.people_1>=100
+and t.people_2>=100)
+or
+(t.people_1>=100
+and  t.people>=100
+and t.people1>=100);
